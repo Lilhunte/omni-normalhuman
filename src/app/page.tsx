5c5912bae64724fb42f461393a5581e5dad036cd
Lilhunte <calistoanselmonhancasse296@gmail.com>
2025-09-05 16:03:50 +0100

    initial commit for clerk

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+
+
+export default async function Home() {
+
+  return <Button className="text-3xl font-bold underline text-amber-300">Home Page</Button>;
+}
