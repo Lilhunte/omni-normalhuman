da5c0d4e282fa827427dd16449d9145ae3c6ceab
Lilhunte <calistoanselmonhancasse296@gmail.com>
2025-09-11 12:26:45 +0100

    imgs remove build again test deployiment

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -15,22 +15,6 @@ export default async function MailPage() {
 
   return (
     <>
-      <div className="md:hidden">
-        <img
-          src="/examples/mail-dark.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="hidden dark:block"
-        />
-        <img
-          src="/examples/mail-light.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="block dark:hidden"
-        />
-      </div>
       <div className="flex-col hidden md:flex h-screen overflow-scroll">
         <div className='absolute flex items-center bottom-4 left-4'>
           <UserButton />
